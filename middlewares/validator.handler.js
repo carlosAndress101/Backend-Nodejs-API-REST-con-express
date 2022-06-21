7834099eefa3e8686cd0949689bf3d4f526c4935
carlosAndress101 <carlos.hinestrozap@unac.edu.co>
2022-06-20 22:06:20 -0500

    correccion de validator

--- middlewares/validator.handler.js
+++ middlewares/validator.handler.js
@@ -1,14 +1,14 @@
 const boom = require('@hapi/boom');
 
-function validatorHandler(schema, property){
-    return (req, res, next) =>{
-        const data = req[property];
-        const { error } = schema.validator(data);
-        if (error){
-           next(boom.badRequest(error));
-        }
-        next();
+function validatorHandler(schema, property) {
+  return (req, res, next) => {
+    const data = req[property];
+    const { error } = schema.validate(data, { abortEarly: false });
+    if (error) {
+      next(boom.badRequest(error));
     }
- }
- 
- module.exports = validatorHandler;
+    next();
+  }
+}
+
+module.exports = validatorHandler;
--- schemas/products.schema.js
+++ schemas/products.schema.js
@@ -1,22 +1,28 @@
-const joi = require('joi');
+const Joi = require('joi');
 
-const id = joi.string().uuid();
-const name = joi.string().alphanum().min(3).max(15);
-const price = joi.number().integer().min(10);
+const id = Joi.string().uuid();
+const name = Joi.string().min(3).max(15);
+const price = Joi.number().integer().min(10);
+const image = Joi.string().uri();
 
-
-const createProductSchemas = joi.object({
-    name: name.required(),
-    price: price.required(),
+const createProductSchemas = Joi.object({
+  name: name.required(),
+  price: price.required(),
+  image: image.required(),
 });
 
-const updateProductSchemas = joi.object({
-    name: name.required(),
-    price: price.required(),
+const updateProductSchemas = Joi.object({
+  name: name.required(),
+  price: price.required(),
+  image: image,
 });
 
-const getProductSchemas = joi.object({
-    id: id.required(),
+const getProductSchemas = Joi.object({
+  id: id.required(),
 });
 
-module.exports = {createProductSchemas, updateProductSchemas, getProductSchemas};
+module.exports = {
+  createProductSchemas,
+  updateProductSchemas,
+  getProductSchemas,
+};
