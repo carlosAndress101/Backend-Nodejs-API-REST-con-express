e8e9ed088f7965e8aa2b9c170d584657ed4bd7e6
carlosAndress101 <carlos.hinestrozap@unac.edu.co>
2022-06-21 13:27:44 -0500

    deploy

--- index.js
+++ index.js
@@ -4,9 +4,20 @@ const routerApi = require('./routes');
 const cors = require('cors');
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler')
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(express.json());
-app.use(cors());
+
+const whitelist = ['http://localhost:3000', 'https://myapp.co'];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('no permitido'));
+    }
+  }
+}
+app.use(cors(options));
 app.get('/', (req, res) => res.send('Hello World!'));
 
 routerApi(app);
