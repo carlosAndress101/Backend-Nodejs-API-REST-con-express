52f645d4bab82fa6226e8fa2d4774b30d087afa9
carlosAndress101 <carlos.hinestrozap@unac.edu.co>
2022-06-26 11:29:56 -0500

    create orders ready

---
+++ services/orders.service.js
@@ -0,0 +1,24 @@
+class OrderService {
+    
+    create(){
+
+    }
+
+    find(){
+        
+    }
+
+    findOne(){
+
+    }
+
+    update(){
+
+    }
+
+    delete(){
+
+    }   
+}
+
+module.exports = OrderService;
