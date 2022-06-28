ad7115a40bbbfa5aebc0c02ba861216fe69b4c6b
carlosAndress101 <carlos.hinestrozap@unac.edu.co>
2022-06-27 22:58:01 -0500

    create config sequelize edit postgres pool

--- libs/postgres.pool.js
+++ libs/postgres.pool.js
@@ -1,12 +1,9 @@
 const { Pool } = require('pg');
+const { config } = require('../config/config');
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPassword);
+const URL = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
-const pool = new Pool({
-    host:'localhost',
-    port:5432,
-    user:'carlos',
-    password:'admin123',
-    database:'my_store'
-});
-
+const pool = new Pool({ connectionString: URL });
 
 module.exports = pool;
