3b1a2c0eb2417db80bc967dd1fd9916caea53443
carlosAndress101 <carlos.hinestrozap@unac.edu.co>
2022-07-01 18:19:23 -0500

    edit sequelize

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -3,17 +3,15 @@ const { config } = require('../config/config');
 const { setupModels } = require('../db/models/index');
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
-const URL = `mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const URL = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
 
 const sequelize = new Sequelize(URL, {
-    dialect: 'mysql',
+    dialect: 'postgres',
     logging: true,
 });
 
 setupModels(sequelize);
-sequelize.sync();
-
 
 
 module.exports = sequelize;
