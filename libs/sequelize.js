6ca4d3231afa66c90b7bfb1e425b5a026ab3348b
carlosAndress101 <carlos.hinestrozap@unac.edu.co>
2022-06-30 19:54:50 -0500

    add and changes

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -3,11 +3,11 @@ const { config } = require('../config/config');
 const { setupModels } = require('../db/models/index');
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
-const URL = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const URL = `mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
 
 const sequelize = new Sequelize(URL, {
-    dialect: 'postgres',
+    dialect: 'mysql',
     logging: true,
 });
 
--- middlewares/error.handler.js
+++ middlewares/error.handler.js
@@ -1,26 +1,28 @@
-/* eslint-disable no-console */
+const { ValidationError } = require('sequelize');
+const boom = require('@hapi/boom');
 
-function logErrors(error, req, res, next){
-    console.error(error);
-    next(error)
+function logErrors(err, req, res, next) {
+  console.error(err);
+  next(err);
+}
+function errorHandler(err, req, res, next) {
+  res.status(500).json({ message: err.message, stack: err.stack });
 }
 
-//middleware manejo de errores con orden y un message y un stack para mayor facilidad.
-function errorHandler(error, req, res, next){
-    res.status(500).json({
-        message: error.message,
-        stack: error.stack,
-    })
-}
-
-
-//middleware para maneja los errores especificos de boom.
-function boomErrorHandler(error, req, res, next){
-   if(error.isBoom){
-    const { output } = error;
+function boomErrorHandler(err, req, res, next) {
+  if (err.isBoom) {
+    const { output } = err;
     res.status(output.statusCode).json(output.payload);
-   }
-   next(error);
+  }
+  next(err);
 }
 
-module.exports = {logErrors, errorHandler, boomErrorHandler};
+function ormErrorHandler(err, req, res, next) {
+  if (err instanceof ValidationError) {
+    res
+      .status(409)
+      .json({ statusCode: 409, message: err.name, errors: err.errors });
+  }
+  next(err);
+}
+module.exports = { logErrors, errorHandler, boomErrorHandler, ormErrorHandler };
