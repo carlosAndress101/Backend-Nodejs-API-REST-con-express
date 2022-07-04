f64f21217547c2d9575f2b08f685263e046b4da6
carlosAndress101 <carlos.hinestrozap@unac.edu.co>
2022-07-04 01:03:34 -0500

    init de customer

--- db/models/index.js
+++ db/models/index.js
@@ -1,11 +1,13 @@
 const { User, userSchema } = require('./user.model');
 const { Products, productSchema } = require('./products.model');
 const {Categories, categorieSchema} = require('./categories.model');
+const {Customer, customerSchema} = require('../models/customer.model');
 
 function setupModels(sequelize){
     User.init(userSchema, User.config(sequelize));
     Products.init(productSchema, Products.config(sequelize));
     Categories.init(categorieSchema, Categories.config(sequelize));
+    Customer.init(customerSchema, Customer.config(sequelize));
 }
 
 module.exports = { setupModels };
