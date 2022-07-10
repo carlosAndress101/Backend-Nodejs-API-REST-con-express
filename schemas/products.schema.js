bdc0260802c805f6abd8f9f7c6353cc8cebffd1b
Carlos Andrés Hinestroza <carlos.hinestrozap@unac.edu.co>
2022-07-10 10:31:10 -0500

    edit-schema de products add new campos

--- schemas/products.schema.js
+++ schemas/products.schema.js
@@ -3,18 +3,25 @@ const Joi = require('joi');
 const id = Joi.string().uuid();
 const name = Joi.string().min(3).max(15);
 const price = Joi.number().integer().min(10);
+const description = Joi.string().min(10);
 const image = Joi.string().uri();
+const categoryId = Joi.number().integer();
+
 
 const createProductSchemas = Joi.object({
   name: name.required(),
   price: price.required(),
   image: image.required(),
+  description: description.required(),
+  categoryId:categoryId.required(),
 });
 
 const updateProductSchemas = Joi.object({
-  name: name.required(),
+  name: name,
   price: price,
   image: image,
+  description:description,
+  categoryId
 });
 
 const getProductSchemas = Joi.object({
