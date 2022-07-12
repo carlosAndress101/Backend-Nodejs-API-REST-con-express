ed383df0d8d51d42c6547e5cdcb92b0660399b10
Carlos Andrés Hinestroza <carlos.hinestrozap@unac.edu.co>
2022-07-11 21:57:19 -0500

    create-products

--- db/models/products.model.js
+++ db/models/products.model.js
@@ -1,6 +1,7 @@
-const { Model, DataTypes, Sequelize } = require('sequelize')
+const { Model, DataTypes, Sequelize } = require('sequelize');
+const { CATEGORIES_TABLE } = require('../models/categories.model');
 
-const PRODUCTS_TABLE = 'products'
+const PRODUCTS_TABLE = 'products';
 
 const productSchema = {
   id: {
@@ -10,38 +11,53 @@ const productSchema = {
     type: DataTypes.INTEGER,
   },
   name: {
-    allowNull: false,
     type: DataTypes.STRING,
+    allowNull: false,
+  },
+  image: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: false,
   },
   price: {
-    allowNull: false,
     type: DataTypes.INTEGER,
-  },
-  imagen: {
     allowNull: false,
-    type: DataTypes.STRING,
   },
-  createAt: {
+  createdAt: {
     allowNull: false,
     type: DataTypes.DATE,
-    field: 'create_at',
+    field: 'created_at',
     defaultValue: Sequelize.NOW,
   },
-}
+  categoriesId: {
+    field: 'category_id',
+    allowNull: false,
+    type: DataTypes.INTEGER,
+    references:{
+      model: CATEGORIES_TABLE,
+      key: 'id'
+    },
+    onUpdate: 'CASCADE',
+    onDelete:'SET NULL',
+  }
+};
 
 class Products extends Model {
-    static associate() {
-        //associate
-    }
+  static associate() {
+    this.belongsTo(Model.categories, { as: 'categories' });
+  }
 
-    static config(sequelize) {
-        return {
-            sequelize,
-            tableName: PRODUCTS_TABLE,
-            modelName: 'products',
-            timestamps: false,
-        }
-    }
+  static config(sequelize) {
+    return {
+      sequelize,
+      tableName: PRODUCTS_TABLE,
+      modelName: 'products',
+      timestamps: false,
+    };
+  }
 }
 
-module.exports = { PRODUCTS_TABLE, productSchema, Products};
+module.exports = { PRODUCTS_TABLE, productSchema, Products };
